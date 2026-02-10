438f1ed3f4ecda3cd165272171420ad6883ff699
Petrov Vadim <vadikdydy@mail.ru>
2026-02-10 08:56:44 +0300

    2

--- CW/100226.cpp
+++ CW/100226.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
-#include <iostream>
+#include <iostream>\
+
+// 1 example
 int* insert(const int* a, size_t s, size_t i, int v)
 {
   int* data = new int[s + 1];
@@ -17,6 +19,23 @@ void insert(int** a, size_t s, size_t i, int v)
   int* data = insert(*a, s, i, v);
   delete[] *a;
   *a = data;
+}
+
+// 2 example
+int* remove(int* a, size_t s, size_t i)
+{
+  int r = a[i];
+  for(size_t j =0; j < s - 1; ++j)
+  {
+    a[j] = a[j - 1];
+  }
+  a[s] = r;
+  return a + s - 1;
+}
+
+int* cut(const int* a, size_t s) //оставить s элементов ДОМА
+{
+
 }
 
 int main()
