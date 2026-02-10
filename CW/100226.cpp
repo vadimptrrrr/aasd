d1bf3189632afe686c74f47c9e5f244088499d86
Petrov Vadim <vadikdydy@mail.ru>
2026-02-10 08:15:32 +0300

    foo insert() done

--- CW/100226.cpp
+++ CW/100226.cpp
@@ -1,5 +1,12 @@
 #include <cstddef>
 int* insert(const int* a, size_t s, size_t i, int v)
 {
-  
+  int* data = new int[s + 1];
+  for(size_t j = 0; j < (s + 1); ++j)
+  {
+    if(j < i) data[j] = a[i];
+    if(j == i) data[j] = v;
+    if(j > i) data[j] = a[j + 1];
+  }
+  return data;
 }
