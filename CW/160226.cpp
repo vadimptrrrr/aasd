010a9cbaa88f36d28da42005b22baa55a6f762b7
Petrov Vadim <vadikdydy@mail.ru>
2026-02-16 16:23:39 +0300

    first done

--- CW/160226.cpp
+++ CW/160226.cpp
@@ -3,5 +3,21 @@
 template < class T >
 T* insert(const T* a, size_t s, size_t i, T v)
 {
-  
+  T* r = new T[s + 1];
+  try
+  {
+    for (size_t k = 0; k < i; ++k)
+    {
+      r[k] = a[k];
+    }
+    r[i] = v;
+    for (size_t k = i+1; k < s+1; ++k)
+    {
+      r[k] = a[k-1]
+    }
+  }
+  catch(...)
+  {
+    throw;
+  }
 }
